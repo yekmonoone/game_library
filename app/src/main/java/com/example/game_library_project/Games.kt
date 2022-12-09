ceb50a34b06e7e423f62c005139d7d24ee241578
Ardabozkurt <ardanbzkrt@gmail.com>
2022-12-09 23:26:29 +0300

    search bar, Games.kt -> recyclerview added

--- app/src/main/java/com/example/game_library_project/Games.kt
+++ app/src/main/java/com/example/game_library_project/Games.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.game_library_project.databinding.FragmentGamesBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,6 +28,7 @@ class Games : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
