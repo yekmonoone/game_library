7673dda78a49be4931742f21b4605b2969abba01
Ardabozkurt <ardanbzkrt@gmail.com>
2022-12-11 17:33:30 +0300

    App Updated recyclerview_item_layout.xml updated fragment_games.xml updated bottom_nav_bar.xml updated Sample games changed

--- app/src/main/java/com/example/game_library_project/Adapter.kt
+++ app/src/main/java/com/example/game_library_project/Adapter.kt
@@ -1,21 +1,58 @@
 package com.example.game_library_project
 
 
+import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.game_library_project.databinding.RecyclerviewItemLayoutBinding
 
 class Adapter(val gameList : ArrayList<GamesInfo>) : RecyclerView.Adapter<Adapter.GameHolder>(){
 
-    class GameHolder(val binding: RecyclerviewItemLayoutBinding) : RecyclerView.ViewHolder(binding.root){
+
+    /*
+
+    CLICK LISTENER
+    private lateinit var mListener: onGameClickListener
+
+
+    interface onGameClickListener{
+
+        fun onGameClick(position: Int)
+
+
+    }
+
+    fun setOnGameClickListener(listener: onGameClickListener){
+
+        mListener = listener
+
+    }
+    */
+
+
+
+    class GameHolder(val binding: RecyclerviewItemLayoutBinding/*,listener: onGameClickListener*/) : RecyclerView.ViewHolder(binding.root){
+
+        /*init {
+
+            itemView.setOnClickListener {
+                listener.onGameClick(adapterPosition)
+            }
+
+        }
+
+         */
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameHolder {
         val binding = RecyclerviewItemLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return GameHolder(binding)
+        return GameHolder(binding/*,mListener*/)
     }
+
+        //private var row_index = -1
 
     override fun onBindViewHolder(holder: GameHolder, position: Int) {
 
@@ -25,6 +62,16 @@ class Adapter(val gameList : ArrayList<GamesInfo>) : RecyclerView.Adapter<Adapte
         holder.binding.gameName.text = gameList.get(position).gameName
         holder.binding.score.text = gameList.get(position).score.toString()
         holder.binding.genre.text = gameList.get(position).genre
+
+
+        /*if (row_index === position) {
+            holder.itemView.setBackgroundColor(Color.parseColor("#E0E0E0"))
+        } else {
+            holder.itemView.setBackgroundColor(Color.parseColor("#ffffff"))
+        }
+
+         */
+
 
     }
 
